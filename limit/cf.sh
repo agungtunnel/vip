5456d8c6ed3c395a47e59fd1f2b3672e41c687db
agungtunnel <148929535+agungtunnel@users.noreply.github.com>
2023-12-21 21:09:48 +0700

    Add files via upload

--- limit/cf.sh
+++ limit/cf.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-MYIP=$(wget -qO- ipv4.icanhazip.com);
+MYIP=$(wget -qO- icanhazip.com);
 apt install jq curl -y
-#read -p "Masukan Domain (contoh : Dragon)" domen
+read -p "Masukan Domain (contoh : Agung)" domen
 DOMAIN=mytun.cloud
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 dns=${sub}.mytun.cloud
 CF_ID=mohagungsetiawan3@gmail.com
 CF_KEY=32c034925166ecc42c3fcb572e14cf3325bc9
 set -euo pipefail
-IP=$(wget -qO- ipv4.icanhazip.com);
+IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${dns}..."
 ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
      -H "X-Auth-Email: ${CF_ID}" \
